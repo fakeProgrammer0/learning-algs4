38ffd6d84b5892ee27e443858722bbdf55478286
fakeProgrammer0 <greenjiachen@gmail.com>
2020-10-12 21:52:23 +0800

    [oj] add LCS

--- src/main/java/com/green/learning/algs4/oj/LongestCommonSubsequences.java
+++ src/main/java/com/green/learning/algs4/oj/LongestCommonSubsequences.java
@@ -11,16 +11,54 @@ public class LongestCommonSubsequences
 {
     private static enum EXTEND_ORIENT
     {FROM_X, FROM_Y, FROM_BOTH}
-    
+
     ;
-    
+
     public static String lcs_dp1(String x, String y)
     {
         final int M = x.length();
         final int N = y.length();
         int[][] lens = new int[M + 1][N + 1];
+
+        for (int i = 1; i <= M; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                if (x.charAt(i - 1) == y.charAt(j - 1))
+                    lens[i][j] = lens[i - 1][j - 1] + 1;
+                else lens[i][j] = Math.max(lens[i - 1][j], lens[i][j - 1]);
+            }
+        }
+
+        Stack<Character> commonSubsequence = new Stack<>();
+        int u = M, v = N;
+        while (u > 0 && v > 0)
+        {
+            if (lens[u][v] == lens[u - 1][v])
+                u--;
+            else if (lens[u][v] == lens[u][v - 1])
+                v--;
+            else
+            {
+                commonSubsequence.push(x.charAt(u - 1));
+                u--;
+                v--;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!commonSubsequence.empty())
+            result.append(commonSubsequence.pop());
+        return result.toString();
+    }
+
+    public static String lcs_dp2(String x, String y)
+    {
+        final int M = x.length();
+        final int N = y.length();
+        int[][] lens = new int[M + 1][N + 1];
         EXTEND_ORIENT[][] orients = new EXTEND_ORIENT[M + 1][N + 1];
-        
+
         for (int i = 1; i <= M; i++)
         {
             for (int j = 1; j <= N; j++)
@@ -40,7 +78,7 @@ public class LongestCommonSubsequences
                 }
             }
         }
-        
+
         Stack<Character> commonSubsequence = new Stack<>();
         int u = M, v = N;
         while (u > 0 && v > 0)
@@ -66,7 +104,7 @@ public class LongestCommonSubsequences
                 }
             }
         }
-        
+
         StringBuilder result = new StringBuilder();
         while (!commonSubsequence.empty())
         {
@@ -74,12 +112,38 @@ public class LongestCommonSubsequences
         }
         return result.toString();
     }
-    
-    private static int[][] lens_r;
-    
-    public  static void lcs_recursive(String x, String y)
+
+    public static int lcs_len(String x, String y)
     {
-    
+        int short_len, long_len;
+        String short_str, long_str;
+        if(x.length() <= y.length())
+        {
+            short_str = x;
+            short_len = x.length();
+            long_str = y;
+            long_len = y.length();
+        }else {
+            short_str = y;
+            short_len = y.length();
+            long_str = x;
+            long_len = x.length();
+        }
+
+        int[][] lens = new int[2][short_len + 1];
+        for(int i = 1; i <= long_len; i++)
+        {
+            int cur_row = i & 1; // cur_row = i % 2
+            int last_row = cur_row ^ 1; // last_row = (i - 1) % 2
+            for(int j = 1; j <= short_len; j++)
+            {
+                if (long_str.charAt(i - 1) == short_str.charAt(j - 1))
+                    lens[cur_row][j] = lens[last_row][j - 1] + 1;
+                else lens[cur_row][j] = Math.max(lens[last_row][j], lens[cur_row][j - 1]);
+            }
+        }
+
+        return lens[long_len & 1][short_len];
     }
-    
+
 }
--- src/test/java/com/green/learning/algs4/oj/LongestCommonSubsequencesTest.java
+++ src/test/java/com/green/learning/algs4/oj/LongestCommonSubsequencesTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * java 1.8 ; junit 5.4
+ */
 class LongestCommonSubsequencesTest
 {
     private static class LCS_TestCase
@@ -11,56 +14,88 @@ class LongestCommonSubsequencesTest
         private String x;
         private String y;
         private String lcs;
-        
+
         public LCS_TestCase(String x, String y, String lcs)
         {
             this.x = x;
             this.y = y;
             this.lcs = lcs;
         }
-        
+
         public String getX()
         {
             return x;
         }
-        
+
         public String getY()
         {
             return y;
         }
-        
+
         public String getLcs()
         {
             return lcs;
         }
     }
-    
-    
+
+
     @Test
     void test_lcs_dp1()
     {
         LCS_TestCase[] testCases = {
                 new LCS_TestCase("BCDAACD", "ACDBAC", "CDAC"),
                 new LCS_TestCase("ABCDGH", "AEDFHR", "ADH"),
-                new LCS_TestCase("ABC","AC","AC")
+                new LCS_TestCase("ABC", "AC", "AC"),
+                new LCS_TestCase("1A2C3D4B56", "B1D23CA45B6A", "123456")
         };
-        
+
         for (LCS_TestCase testCase : testCases)
         {
-            assertEquals(testCase.getLcs(),
-                    LongestCommonSubsequences.lcs_dp1(testCase.getX(), testCase.getY()));
+            String actual_lcs = LongestCommonSubsequences.lcs_dp1(testCase.getX(), testCase.getY());
+            System.out.printf("x = %s ; y = %s ; \ntarget lcs = %s , length = %d\noutput lcs = %s , length = %d\n\n",
+                    testCase.getX(), testCase.getY(),
+                    testCase.getLcs(), testCase.getLcs().length(),
+                    actual_lcs, actual_lcs.length());
+            assertEquals(testCase.getLcs().length(), actual_lcs.length());
+
+            assertEquals(testCase.getLcs().length(),
+                    LongestCommonSubsequences.lcs_dp2(testCase.getX(), testCase.getY()).length());
         }
     }
-    
+
+    @Test
+    void test_lcs_len()
+    {
+        LCS_TestCase[] testCases = {
+                new LCS_TestCase("BCDAACD", "ACDBAC", "CDAC"),
+                new LCS_TestCase("ABCDGH", "AEDFHR", "ADH"),
+                new LCS_TestCase("ABC", "AC", "AC"),
+                new LCS_TestCase("1A2C3D4B56", "B1D23CA45B6A", "123456")
+        };
+
+        for (LCS_TestCase testCase : testCases)
+        {
+            int actual_lcs_len = LongestCommonSubsequences.lcs_len(testCase.getX(), testCase.getY());
+            System.out.printf("x = %s ; y = %s ; \ntarget lcs length = %d\noutput lcs length = %d\n\n",
+                    testCase.getX(), testCase.getY(), testCase.getLcs().length(), actual_lcs_len);
+            assertEquals(testCase.getLcs().length(), actual_lcs_len);
+
+//            assertEquals(testCase.getLcs(),
+//                    LongestCommonSubsequences.lcs_dp2(testCase.getX(), testCase.getY()));
+//            assertEquals(testCase.getLcs().length(),
+//                    LongestCommonSubsequences.lcs_len(testCase.getX(), testCase.getY()));
+        }
+    }
+
     @Test
     void test_lcs_rec()
     {
         LCS_TestCase[] testCases = {
                 new LCS_TestCase("BCDAACD", "ACDBAC", "CDAC"),
                 new LCS_TestCase("ABCDGH", "AEDFHR", "ADH"),
-                new LCS_TestCase("ABC","AC","AC")
+                new LCS_TestCase("ABC", "AC", "AC")
         };
-    
+
         for (int t = 0; t < testCases.length; t++)
         {
             System.out.printf("test case [%d]\n", t);
